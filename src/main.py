25659cee57aea487c025f770b6df9da1a7419e88
readulislamtamim <readulislamtamim@gmail.com>
2026-05-01 22:51:50 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ today = datetime.date.today()
 print(f"Today's date is {today}")
 
 #%%
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Add:", add(5, 10))
 print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(8, 7))
