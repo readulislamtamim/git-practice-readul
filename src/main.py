fcbf1e4e2ba54b4e3d0ba11ba08fe638a0ddcf89
readulislamtamim <readulislamtamim@gmail.com>
2026-05-01 22:28:30 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -5,3 +5,9 @@ print("My name is Md. Readul Islam")
 
 today = datetime.date.today()
 print(f"Today's date is {today}")
+
+#%%
+from utils import add, subtract
+
+print("Add:", add(5, 10))
+print("Subtract:", subtract(10, 5))
